61b956859845c544b26ff88f1f816510d777aeb8
Zach Woolboy <zachtwoolboy@gmail.com>
2019-06-01 12:25:29 -0600

    updated comment controller and added belongs to movie

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,6 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only:[:show, :edit,:update, :destroy]
+  before_action :set_movie
 
   def index
     @comments = Comment.all
@@ -44,12 +45,13 @@ class CommentsController < ApplicationController
   private
   
   def set_comment
-    @comment= Comment.find(params[:id])
+    @comment = Comment.find(params[:id])
   end
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
 
   def comment_params
     params.require(:comment).permit(:body, :user_id, :movie_id)
   end
 end
-
-puts "seeded"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_01_173018) do
+ActiveRecord::Schema.define(version: 2019_06_01_180742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_06_01_173018) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.bigint "movie_id"
+    t.index ["movie_id"], name: "index_comments_on_movie_id"
   end
 
   create_table "movies", force: :cascade do |t|
@@ -44,4 +46,5 @@ ActiveRecord::Schema.define(version: 2019_06_01_173018) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "movies"
 end
